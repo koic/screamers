0822d3e4189558f9d56bc5c0dd86349ffcbffb1c
Koichi ITO <koic.ito@gmail.com>
2017-06-28 16:23:00 +0900

    Add warning log

--- lib/screamers/schema_collector.rb
+++ lib/screamers/schema_collector.rb
@@ -14,7 +14,12 @@ module Screamers
       tables.delete('ar_internal_metadata')
 
       @target_tables = tables.each_with_object({}) {|table, target|
-        columns = Module.const_get(table.classify).columns rescue next
+        begin
+          columns = Module.const_get(table.classify).columns
+        rescue
+          puts "An ActiveRecord model mapped to `#{table}` foo could not be found. Please check if you need handmade by yourself."
+          next
+        end
 
         target_columns = columns.select {|column|
           column.type == @old_column_type
