6f65156af45322d3c8d2b6af964fe4146b1344f2
Koichi ITO <koic.ito@gmail.com>
2018-11-04 18:16:24 +0900

    CI with the supported Rails versions

--- spec/rails/generators/screamers/migration/migration_generator_spec.rb
+++ spec/rails/generators/screamers/migration/migration_generator_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Screamers::Generators::MigrationGenerator, type: :generator do
   arguments ['integer', 'bigint']
 
   let(:generated_code) { <<-EOS.strip_heredoc
-    class ChangeIntegerToBigintUsingScreamers < ActiveRecord::Migration[5.2]
+    class ChangeIntegerToBigintUsingScreamers < ActiveRecord::Migration[#{ActiveRecord::Migration.current_version}]
       def up
         change_column :comments, :id, :bigint
         change_column :comments, :post_id, :bigint
